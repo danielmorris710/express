07fae91ed070b08561022f2a63ee23cf4854ba12
Daniel Morris <uptownmelo#gmail.com>
2022-07-27 14:25:53 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/greeting/:name', function(req, res) {
     res.send(req.params.name + '!' + ' It\'s so great to see you!');
   })
 
+  app.get('/tip/:total/:tipPercentage',(req,res)=>{
+    let tip = parseInt(req.params.total)*(parseInt(req.params.tipPercentage)/100);
+    res.send(`${tip}`);
+})
+
 app.listen(port,() => {
     console.log('I am listening on port' , port);
 })
